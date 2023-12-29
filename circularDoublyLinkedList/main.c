81eb5e68deb5ec0fa5f0404e1dc8aaf6c53cbfcd
JohnWSweeney <JohnWSweeney@gmail.com>
2023-12-29 11:28:35 -0800

    Add project files.

---
+++ circularDoublyLinkedList/main.c
@@ -0,0 +1,13 @@
+#include "circularDoublyLinkedList.h"
+
+int main()
+{
+	printf("Circular Doubly Linked List v0.0.0\n\n");
+	int result = 0;
+	struct dNode* list = NULL;
+
+	/// test code here: ////////////////////////////////////////
+	
+	printf("Result: %d\n", result);
+	////////////////////////////////////////////////////////////
+}
---
+++ circularDoublyLinkedList/nodes.h
@@ -0,0 +1,8 @@
+#pragma once
+
+struct dNode
+{
+	int data;
+	struct dNode* next;
+	struct dNode* prev;
+};
